18dcf1a3e82b05d8747012ece2e59d2acc6b4a25
Malte Dömer <maltedoemer@gmail.com>
2021-03-06 15:53:31 +0100

    added string.h definitions

--- include/libc/string.h
+++ include/libc/string.h
@@ -7,7 +7,47 @@ extern "C" {
 #include "stdint.h"
 #include "stddef.h"
 
-void* memcpy(void* dest, const void* src, size_t num);
+void* memccpy(void* s1, const void* s2, int c, size_t n);
+
+const void* memchr(const void* ptr, int value, size_t n);
+
+int memcmp(const void* ptr1, const void* ptr2, size_t n);
+
+void* memcpy(void* dest, const void* src, size_t n);
+
+void* memmove(void* dest, const void* src, size_t n);
+
+void* memset(void* dest, int value, size_t n);
+
+char* strcat(char* dest, const char* src);
+
+char* strchr(const char* str, int value);
+
+int strcmp(const char* str1, const char* str2);
+
+char* strcpy(char* dest, const char* src);
+
+size_t strcspn(const char* str1, const char* str2);
+
+char* strdup(const char* str);
+
+size_t strlen(const char* str);
+
+char* strncat(char* dest, const char* src, size_t n);
+
+int strncmp(const char* str1, const char* str2, size_t n);
+
+char* strncpy(char* dest, const char* src, size_t n);
+
+char* strpbrk(const char* str1, const char* str2);
+
+char* strrchr(const char* str, int value);
+
+size_t strspn(const char* str1, const char* str2);
+
+char* strstr(const char* str1, const char* str2);
+
+char* strtok(char* str, const char* delims);
 
 #ifdef __cplusplus
 }
