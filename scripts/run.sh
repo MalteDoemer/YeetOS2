98ce66c1fdf1cc77f9d2aa54c585e4b8abbdd64a
Malte Dömer <maltedoemer@gmail.com>
2021-04-04 14:32:40 +0200

    run qemu with accelerators

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ PART=/dev/loop1
 MNT=/mnt/vdisk
 KERNEL=YeetOS
 
-qemu-system-i386.exe -drive format=raw,file=\\\\wsl\$\\Ubuntu$IMAGE,if=ide -m 512 -name "YeetOS" -serial stdio -k ch
+qemu-system-i386.exe -accel whpx -drive format=raw,file=\\\\wsl\$\\Ubuntu$IMAGE,if=ide -m 512 -name "YeetOS" -serial stdio -k ch
