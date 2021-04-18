cfef29654d78f7c7a47ddee0efab50d5972a9245
Malte Dömer <maltedoemer@gmail.com>
2021-04-18 08:56:44 +0200

    added strnlen strcmp and strncmp

--- Arch/x86/LibYT/StdLib.cpp
+++ Arch/x86/LibYT/StdLib.cpp
@@ -90,9 +90,36 @@ size_t strlen(const char* str)
     return str - start;
 }
 
-// size_t strnlen(const char* str, size_t maxlen) {}
+size_t strnlen(const char* str, size_t maxlen)
+{
+    size_t counter = maxlen;
+    while (*str && counter) { counter--, str++; }
+    return maxlen - counter;
+}
 
-// int strcmp(const char* str1, const char* str2) {}
+int strcmp(const char* str1, const char* str2)
+{
+    for (;; str1++, str2++) {
+        if (*str1 != *str2)
+            break;
 
-// int strncmp(const char* str1, const char* str2, size_t n) {}
+        if (*str1 == '\0' || *str2 == '\0')
+            break;
+    }
+
+    return *str1 - *str2;
+}
+
+int strncmp(const char* str1, const char* str2, size_t n)
+{
+    for (; n--; str1++, str2++) {
+        if (*str1 != *str2)
+            return *str1 - *str2;
+
+        if (*str1 == '\0' || *str2 == '\0')
+            return *str1 - *str2;
+    }
+
+    return 0;
+}
 }
