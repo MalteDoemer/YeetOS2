155e5fa255b71fec00c46025d6a3a81a80181f7b
Malte Dömer <maltedoemer@gmail.com>
2021-04-19 17:51:44 +0200

    new test

---
+++ Kernel/Tests/GerneralTests.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Malte Dömer
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "Types.hpp"
+#include "Kernel/Kernel.hpp"
+#include "Kernel/KernelTests.hpp"
+
+namespace Kernel::Tests {
+
+static constexpr int data = 0xaa;
+
+static int test_int = 0;
+
+struct GlobalTest {
+    GlobalTest() { test_int = data; }
+};
+
+GlobalTest global_test;
+
+TEST_CASE(global_constructors)
+{
+    EXPECT_EQU(test_int, data);
+    return true;
+}
+
+}
